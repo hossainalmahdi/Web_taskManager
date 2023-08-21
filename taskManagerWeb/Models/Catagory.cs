642c710e7dfa09581e55d0f30819aee5a7563544
hossainalmahdi <hame.mahadi@gmail.com>
2023-08-21 20:58:50 +0600

    Added Catagory file in Model

---
+++ taskManagerWeb/Models/Catagory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskManagerWeb.Models;
+public class Catagory
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    
+}
