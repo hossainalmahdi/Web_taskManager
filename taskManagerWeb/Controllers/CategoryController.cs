95fdb053164420bec6427103d3c61198de94fd35
hossainalmahdi <hame.mahadi@gmail.com>
2023-08-23 02:18:43 +0600

    for Create page{ Added Server Side validation Added Client Side Validation }

--- taskManagerWeb/Controllers/CategoryController.cs
+++ taskManagerWeb/Controllers/CategoryController.cs
@@ -16,8 +16,22 @@ namespace taskManagerWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //GET
         public IActionResult Create()
         {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid) //server side validation
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }   
             return View();
         }
     }
