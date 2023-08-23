cc4da84e1ebb683d5745f574f23c73538aa16544
hossainalmahdi <hame.mahadi@gmail.com>
2023-08-23 17:37:38 +0600

    Added delete functionality Basic project completion

--- taskManagerWeb/Controllers/CategoryController.cs
+++ taskManagerWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using taskManagerWeb.Data;
 using taskManagerWeb.Models;
 
@@ -88,6 +89,40 @@ namespace taskManagerWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        //-----------------------Delete-----------------------
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+           
+             _db.Categories.Remove(obj);
+             _db.SaveChanges();
+             return RedirectToAction("Index");
         }
     }
 }
