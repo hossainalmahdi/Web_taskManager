5f059b26ae77103dee509152b25bcac46f788d68
hossainalmahdi <hame.mahadi@gmail.com>
2023-08-23 16:38:41 +0600

    >More Usr/client SIte Validation >Added Edit/Update Functionality

--- taskManagerWeb/Controllers/CategoryController.cs
+++ taskManagerWeb/Controllers/CategoryController.cs
@@ -16,6 +16,7 @@ namespace taskManagerWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //-----------------------CREATE-----------------------
         //GET
         public IActionResult Create()
         {
@@ -26,12 +27,66 @@ namespace taskManagerWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("Name", "The Name field cannot be the same as the Display Order field.");
+            
+            }
+            if (obj.Name == obj.Comments.ToString())
+            {
+                ModelState.AddModelError("Name", "The Name field cannot be the same as the Display Order field.");
+                ModelState.AddModelError("Comments", "The Task Notes field cannot be the same as the Name field.");
+
+            }
             if (ModelState.IsValid) //server side validation
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }   
+            return View();
+        }
+
+        //-----------------------EDIT-----------------------
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Name field cannot be the same as the Display Order field.");
+
+            }
+            if (obj.Name == obj.Comments.ToString())
+            {
+                ModelState.AddModelError("Name", "The Name field cannot be the same as the Display Order field.");
+                ModelState.AddModelError("Comments", "The Task Notes field cannot be the same as the Name field.");
+
+            }
+            if (ModelState.IsValid) //server side validation
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- taskManagerWeb/Models/Category.cs
+++ taskManagerWeb/Models/Category.cs
@@ -5,11 +5,18 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+    
     [Required]
     public string Name { get; set; }
+    
+    [Display(Name = "Display Order")]
+    [Range(1,100,ErrorMessage ="Must Be Between 1 to 100 only!")]
     public int DisplayOrder { get; set; }
+    
     [Required]
+    [Display(Name = "Task Notes")]
     public string Comments { get; set; }
+    
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     
 }
